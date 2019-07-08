187e11580d4d1244901cfccca7997d948fe303ab
GovernmentHack <none>
2019-07-07 21:10:26 -0400

    Refactor command handling

--- src/discordBot/onMessageHandler.ts
+++ src/discordBot/onMessageHandler.ts
@@ -7,6 +7,48 @@ enum Command {
   SETFREQ
 }
 
+const handleLearnCommand = async (bot : DiscordBot, message : Message) => {
+  bot.ingestChannelMessages(message.channel as TextChannel).then((output : IIngestChannelMessagesOutput) => {
+    if(!!output.error) message.channel.send(`I could not ingest messages: ${output.error}`)
+    message.channel.send(`Ingested ${output.messagesIngested} messages.`)
+  })
+}
+
+const handleSetFreqCommand = (bot: DiscordBot, message : Message, options : string[]) => {
+  const isValidCommand = (
+    !!options &&
+    options.length === 1 && 
+    parseInt(options[0]) >= 0 &&
+    parseInt(options[0]) <= 100 
+  )
+
+  if(!isValidCommand) {
+    message.channel.send("!setFreq usage: `!setFreq [0-100]`")
+    return
+  }
+
+  bot.setResponseFrequency(parseInt(options[0]))
+  message.channel.send(`I will respond to ${options[0]}% of messages now!`)
+}
+
+const handleCommands = async (command : Command, options : string[], bot: DiscordBot, message: Message) => {
+  console.info(`Recieved ${Command[command]} command`)
+
+  options = message.cleanContent.split(" ").slice(1)
+  console.info(`Command Options: ${options}`)
+
+  switch(command) {
+    case (Command.LEARN) : {
+      await handleLearnCommand(bot, message)
+      break
+    }
+    case (Command.SETFREQ) : {
+      handleSetFreqCommand(bot, message, options)
+      break
+    }
+  }
+}
+
 const onMessageHandler = async (bot: DiscordBot, message : Message) => {
   let clientWasMentioned : boolean = message.isMemberMentioned(message.client.user) 
   let commandCalled : Command
@@ -16,38 +58,9 @@ const onMessageHandler = async (bot: DiscordBot, message : Message) => {
   if (message.author.bot) return
   
   commandCalled = Command[message.cleanContent.split(" ")[0].slice(1).toUpperCase()]
+  commandOptions = message.cleanContent.split(" ").slice(1)
   if(!!commandCalled) {
-    console.debug(`Recieved ${Command[commandCalled]} command`)
-
-    commandOptions = message.cleanContent.split(" ").slice(1)
-    console.debug(`Command Options: ${commandOptions}`)
-
-    switch(commandCalled) {
-      case (Command.LEARN) : {
-        await bot.ingestChannelMessages(message.channel as TextChannel).then((output : IIngestChannelMessagesOutput) => {
-          if(!!output.error) message.channel.send(`I could not ingest messages: ${output.error}`)
-          message.channel.send(`Ingested ${output.messagesIngested} messages.`)
-        })
-        break
-      }
-      case (Command.SETFREQ) : {
-        const isValidCommand = (
-          !!commandOptions &&
-          commandOptions.length === 1 && 
-          parseInt(commandOptions[0]) >= 0 &&
-          parseInt(commandOptions[0]) <= 100 
-        )
-
-        if(!isValidCommand) {
-          message.channel.send("!setFreq usage: `!setFreq [0-100]`")
-          return
-        }
-
-        bot.setResponseFrequency(parseInt(commandOptions[0]))
-        message.channel.send(`I will respond to ${commandOptions[0]}% of messages now!`)
-        break
-      }
-    }
+    await handleCommands(commandCalled, commandOptions, bot, message)
   }
 
   if(clientWasMentioned && !commandCalled) {

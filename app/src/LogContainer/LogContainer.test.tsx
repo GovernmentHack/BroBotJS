0ebff930d4b63d3b4f8151dfb8dbce0d332bad2b
GovernmentHack <none>
2019-09-03 19:50:57 -0400

    Logs displayed as list of expansions  - NEEDS STYLING BADLY

--- app/src/LogContainer/LogContainer.test.tsx
+++ app/src/LogContainer/LogContainer.test.tsx
@@ -1,10 +1,39 @@
 import React from "react"
 import { shallow, ShallowWrapper } from "enzyme"
 import LogContainer from "./LogContainer"
+import ExpansionPanel from "@material-ui/core/ExpansionPanel"
 import chai from 'chai';
 const expect = chai.expect
 
 describe("LogContainer", () => {
-  
-  
+  let wrapper : ShallowWrapper
+  let dummyLog
+
+  beforeEach(() => {
+    dummyLog = [{
+      id: 10,
+      messageString: "test",
+      messageLinks: [{
+        "key": {
+          "first": "test",
+          "second": ""
+        },
+        "nodes": [
+          {
+            "weight": 1,
+            "probability": [0, 1],
+            "next": ""
+          }
+        ],
+        "weightTotal": 1
+      }],
+      triggerMessage: "some message"
+    }]
+    wrapper = shallow(<LogContainer log={dummyLog}/>)
+  })
+
+  it("Has the message sent displayed as a label", () => {
+    expect(wrapper.find(ExpansionPanel).text()).to.include("test")
+  })
+
 })
--- app/src/LogContainer/LogContainer.tsx
+++ app/src/LogContainer/LogContainer.tsx
@@ -1,14 +1,40 @@
 import React from 'react'
+import ExpansionPanel from '@material-ui/core/ExpansionPanel'
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
 import "./LogContainer.scss"
 
+interface ILogEntry {
+  id: number;
+  messageString: string;
+  messageLinks: any[];
+  triggerMessage: string;
+}
+
 interface ILogContainerProps {
-  log : any[]
+  log : ILogEntry[]
 }
 
 const LogContainer : React.FC<ILogContainerProps> = (props) => {
+
+  const logList = props.log.map((entry) => 
+    <ExpansionPanel key={entry.id.toString()}>
+      <ExpansionPanelSummary
+        expandIcon={<ExpandMoreIcon />}
+      >
+        <p>{entry.messageString}</p>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <p>{`ID: ${entry.id}`}</p>
+        <p>{`Trigger Message: ${entry.triggerMessage}`}</p>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+  )
+
   return (
     <div className="log-container">
-      {JSON.stringify(props.log)}
+      {logList}
     </div>
   )
 }
